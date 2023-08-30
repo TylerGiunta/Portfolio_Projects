7b8b203e6182fc947c7d1be1ba5dda065356d44d
TylerGiunta <142836113+TylerGiunta@users.noreply.github.com>
2023-08-29 21:43:12 -0400

    Update Bellabeat SQL
    Cleaned up the code to make it easier to read

--- SQL_Finished_Product.sql
+++ SQL_Finished_Product.sql
@@ -10,36 +10,43 @@
 		Sleep_day
 		weightLoginfo
 		Selected the top 1000 rows to make sure the import went smoothly */
-			SELECT TOP(1000) *
-			FROM [dbo].[daily_activity_cleaned]
+
+		SELECT TOP(1000) *
+		FROM [dbo].[daily_activity_cleaned]
 
 		--Create a new clean database by changing string data to floats and integers
-			CREATE TABLE bellabeat.dbo.daily_activity_cleaned
+				
+		CREATE TABLE bellabeat.dbo.daily_activity_cleaned
 			(Id FLOAT, ActivityDate DATETIME2(7), TotalSteps INT, TotalDistance FLOAT, VeryActiveDistance FLOAT, ModeratelyActiveDistance FLOAT, 
 			LightActiveDistance FLOAT, SedentaryActiveDistance FLOAT, VeryActiveMinutes INT, FairlyActiveMinutes INT, LightlyActiveMinutes INT, 
 			SedentaryMinutes INT, Calories FLOAT,)
 
 			--Insert the data into the new database
-			INSERT INTO bellabeat.dbo.daily_activity_cleaned
-				(Id,ActivityDate, TotalSteps, TotalDistance, VeryActiveDistance, ModeratelyActiveDistance, LightActiveDistance, SedentaryActiveDistance, VeryActiveMinutes,
-				FairlyActiveMinutes, LightlyActiveMinutes, SedentaryMinutes, Calories)
+				
+		INSERT INTO bellabeat.dbo.daily_activity_cleaned
+			(Id,ActivityDate, TotalSteps, TotalDistance, VeryActiveDistance, ModeratelyActiveDistance, LightActiveDistance, SedentaryActiveDistance, VeryActiveMinutes,
+			FairlyActiveMinutes, LightlyActiveMinutes, SedentaryMinutes, Calories)
+				
 		--Changing data to float
-			SELECT 
-				Id,
-				ActivityDate,
-				TotalSteps,
-				CAST(TotalDistance AS FLOAT) AS TotalDistance,
-				CAST(VeryActiveDistance AS FLOAT) AS VeryActiveDistance,
-				CAST(ModeratelyActiveDistance AS FLOAT) AS ModeratelyActiveDistance,
-				CAST(LightActiveDistance AS FLOAT) AS LightActiveDistance,
-				CAST(SedentaryActiveDistance AS FLOAT) AS SedentaryActiveDistance,
-					VeryActiveMinutes, FairlyActiveMinutes, LightlyActiveMinutes,
-					SedentaryMinutes, Calories
+				
+		SELECT 
+			Id,
+			ActivityDate,
+			TotalSteps,
+			CAST(TotalDistance AS FLOAT) AS TotalDistance,
+			CAST(VeryActiveDistance AS FLOAT) AS VeryActiveDistance,
+			CAST(ModeratelyActiveDistance AS FLOAT) AS ModeratelyActiveDistance,
+			CAST(LightActiveDistance AS FLOAT) AS LightActiveDistance,
+			CAST(SedentaryActiveDistance AS FLOAT) AS SedentaryActiveDistance,
+				VeryActiveMinutes, FairlyActiveMinutes, LightlyActiveMinutes,
+				SedentaryMinutes, Calories
 			FROM bellabeat.dbo.dailyActivity
+				
 		--Ran into the error of a duplicate table, had to run
+				
 			IF EXISTS(SELECT*
-							FROM bellabeat.dbo.daily_activity_cleaned)
-							DROP TABLE bellabeat.dbo.daily_activity_cleaned
+				FROM bellabeat.dbo.daily_activity_cleaned)
+				DROP TABLE bellabeat.dbo.daily_activity_cleaned
 
 		/*At the beginning of the prompt to be able to create the new table 
 
@@ -58,7 +65,7 @@
 		Average Calories
 		Average Active Minutes- Average Active Hours
 		
-	1) Tracking their physical activities and number of users
+		1) Tracking their physical activities and number of users
 		Number of Users 33
 		Average Steps 7637
 		Average Distance 5.48
@@ -71,11 +78,12 @@
 			AVG(Calories) AS average_calories
 		FROM bellabeat.dbo.daily_activity_cleaned
 
-/*2) Tracking Heart Rate
+		/*2) Tracking Heart Rate
 		Users Tracking Heartrate 14
 		Average Heartrate 77BPM
 		Minimum Heartrate 36BPM
 		Maximum Heartrate 203BPM*/
+				
 		SELECT 
 			COUNT(DISTINCT Id) AS users_tracking_heartRate,
 			AVG(Value) AS average_heartrate,
@@ -83,11 +91,12 @@
 			MAX(Value) AS maximum_heartrate
 		FROM bellabeat.dbo.heartrate_seconds
 
-/*3) Tracking Sleep
+		/*3) Tracking Sleep
 		Users Tracking Sleep 24
 		Average Hours of Sleep 6.98
 		Minimum Hourse of Sleep 13.26
 		Average Hours in Bed 7.63*/
+				
 		SELECT 
 			COUNT(DISTINCT Id) AS users_tracking_sleep,
 			AVG(TotalMinutesAsleep)/60.0 AS average_hours_asleep,
@@ -96,7 +105,7 @@
 			AVG(TotalTimeInBed)/60.0 AS average_hours_inBed
 		FROM bellabeat.dbo.sleep_day
 
-/*4) Tracking Weight
+		/*4) Tracking Weight
 		Users Tracking Weight 8
 		Average Weight 72.03
 		Minimum Weight 52.59
@@ -109,7 +118,7 @@
 			MAX(WeightKg) AS maximum_weight
 		FROM bellabeat.dbo.weight_cleaned
 
-/*5) Tracking Steps, Distance, Calories
+		/*5) Tracking Steps, Distance, Calories
 		Average Steps 7637
 		Average Distance 5.48
 		Average Calories 2303.60*/
@@ -121,8 +130,8 @@
 			AVG(Calories) AS average_calories
 		FROM bellabeat.dbo.daily_activity_cleaned
 
-/*6) Calculate the number of days each user tracked physical activity
-	To find minimum value and maximum value add DISTICT on the end
+		/*6) Calculate the number of days each user tracked physical activity
+		To find minimum value and maximum value add DISTICT on the end
 		Max days recording physical activity -31
 		Min days recording physical activity -4*/
 
@@ -131,7 +140,7 @@
 		FROM bellabeat.dbo.daily_activity_cleaned
 			ORDER BY days_activity_recorded
 
-/*7) Calcualate the average minutes of activity
+		/*7) Calcualate the average minutes of activity
 		Average very active minutes 21
 		Average fairly active minutes 13
 		Average lightly active minutes 192
@@ -144,7 +153,7 @@
 			AVG(SedentaryMinutes) AS AverageSedentaryMinutes
 		FROM bellabeat.dbo.daily_activity_cleaned
 
-/*9) Change minutes to hours
+		/*9) Change minutes to hours
 		Average very active hours 0.35
 		Average fairly active hours 0.22
 		Average lightly active hours 3.20
@@ -157,8 +166,8 @@
 			ROUND(AVG(SedentaryMinutes)/60.0,2) AS AverageSedentaryHours
 		FROM bellabeat.dbo.daily_activity_cleaned
 
-/*10) High intensity or high METs implies more people are physically active during that time
+		/*10) High intensity or high METs implies more people are physically active during that time
 
-Change the TotalIntensity into an integer from nvarchar50*/
+		Change the TotalIntensity into an integer from nvarchar50*/
 
-ALTER TABLE bellabeat.dbo.hourly_activity ALTER COLUMN TotalIntensity int
+		ALTER TABLE bellabeat.dbo.hourly_activity ALTER COLUMN TotalIntensity int
